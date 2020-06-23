cbf0c32c98e9d14c2304430be02f2b094212aac1
helderspadua <64776528+helderspadua@users.noreply.github.com>
2020-06-23 19:59:50 +0100

    initial test case

---
+++ src/YouTube.End2End.Selenium/YouTube.End2End.Selenium/PageObjects/SearchPage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace YouTube.End2End.Selenium.PageObjects
+{
+    class SearchPage
+    {
+        private IWebDriver driver;
+
+        public SearchPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        internal bool HasAvailableTitles(String title)
+        {
+
+            var availableTitlesElements = driver.FindElements(By.ClassName("title-and-badge"))
+                .Select(t => t.FindElement(By.TagName("a")))
+                .Select(t => t.GetAttribute("title"));
+              
+                
+
+            return availableTitlesElements.Any(t => t.Contains(title));
+                
+            
+        }
+    }
+}
